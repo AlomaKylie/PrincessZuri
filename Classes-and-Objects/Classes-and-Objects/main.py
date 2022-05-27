1918bb97caeeea4040eed0da31bcf0134f160708
AlomaKylie <105173468+AlomaKylie@users.noreply.github.com>
2022-05-27 14:38:45 +0100

    Add files via upload

---
+++ Classes-and-Objects/Classes-and-Objects/main.py
@@ -0,0 +1,24 @@
+
+class Student:
+    def __init__(self, name, age, tracks, score):
+        self.name = name
+        self.age = age
+        self.tracks = tracks
+        self.score = score
+
+@classmethod
+def change_name(self, new_name):
+   self.name = new_name
+
+def change_age(self, new_age):
+    if isinstance(new_age, int()):
+        self.age = new_age
+
+def change_track(self, new_track):
+    self.tracks.append (new_track)
+
+def get_score(self):
+    return self.score
+
+
+
